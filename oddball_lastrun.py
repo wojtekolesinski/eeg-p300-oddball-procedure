1a2d6294997354c9d631af581be8f1493e903ef4
wojtekolesinski <wojtek.olesinski00@gmail.com>
2021-12-16 19:18:22 +0100

    changed number of loops

--- oddball_lastrun.py
+++ oddball_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
-    on grudzień 16, 2021, at 13:33
+    on grudzień 16, 2021, at 19:10
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
